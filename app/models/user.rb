d2fa0cafc0f3a322a71c75f8d4c7eb199b87ec64
Antoine Sakho <antoine.sakho@gmail.com>
2013-08-19 18:28:55 +0200

    deleted validates_presence_of_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
-  #all users must have names
-  validates_presence_of :name 
+
 end
