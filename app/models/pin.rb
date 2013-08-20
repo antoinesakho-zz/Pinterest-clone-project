5568983899f6bfc51812ba4d7583c6828e19cd8c
Antoine Sakho <antoine.sakho@gmail.com>
2013-08-20 07:18:24 +0200

    Added images to pin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,13 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
-  validates :description, presence: true, length: {maximum: 140}
+  attr_accessible :description, :image
+  
 
-  belongs_to :user
+  validates :description, presence: true, length: {maximum: 140}
   validates :user_id, presence: true
+  has_attached_file :image, styles: { medium: "320x240"}
+  validates_attachment :image, presence: true,
+  								content_type: {content_type: ['image/jpg', 'image/jpeg', "image/png", "image/gif"]},
+  								size: {less_than: 5.megabytes}
+  belongs_to :user
+  
 end
