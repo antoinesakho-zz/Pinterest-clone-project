85f738ce0de17421052f5a555e39d7f7fad408ff
Antoine Sakho <antoine.sakho@gmail.com>
2013-08-22 22:09:57 +0200

    remoteurl

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,13 +1,18 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
   
 
   validates :description, presence: true, length: {maximum: 140}
   validates :user_id, presence: true
   has_attached_file :image, styles: { medium: "320x240"}
-  validates_attachment :image, presence: true,
+  validates_attachment :image, 
   								content_type: {content_type: ['image/jpg', 'image/jpeg', "image/png", "image/gif"]},
   								size: {less_than: 5.megabytes}
   belongs_to :user
+
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  	super
+  end
   
 end
